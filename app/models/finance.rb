dd2c8e91e068d6c4f9a7929176160b67ff5036af
Ishan Subedi <ikushum@gmail.com>
2016-10-25 22:23:06 +0545

    Modified the model file

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -1,3 +1,23 @@
 class Finance < ActiveRecord::Base
     validates :title, :amount, presence: true
+    
+  def self.debt
+    debt = 0
+    all.each do |finance|
+        debt += finance.amount if finance.amount < 0
+    end
+    return debt
+  end
+  
+  def self.balance
+      balance=0
+      all.each do |finance|
+          balance+=finance.amount if finance.amount>=0 
+      end
+   return balance
+  end
+
+  def self.total
+    return balance+debt
+  end
 end
